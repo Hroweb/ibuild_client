15b77c707d5a45db17df3a0f4badf3b725a3fe71
Hroweb <hroweb89@gmail.com>
2024-06-11 18:13:40 +0400

    Template1 description to editor

--- client/src/components/(Admin)/RightBar/portfolio/templates/template1.jsx
+++ client/src/components/(Admin)/RightBar/portfolio/templates/template1.jsx
@@ -2,20 +2,15 @@ import styles from "@/components/(Admin)/RightBar/RightBar.module.scss"
 import ImageUploadBlock from "@/components/(Admin)/CaseSingle/ImageUploadBlock"
 import TitleBlock from "@/components/(Admin)/TitleBlock/TitleBlock"
 import DescBlock from "@/components/(Admin)/DescBlock/DescBlock"
+import QuillComponent from "@/components/(Admin)/QuillComponent/QuillComponent";
 
 const Template1 = ({ id, handleInputChange, formData, handleMediaInputChange, selectedMedia, isEdit = false }) => {
-    /*console.log(id);
-    console.log(title);
-    console.log(formData.templateFields);*/
     const templateFields = isEdit ? formData : (formData.templateFields[id] && formData.templateFields[id].Template1 ? formData.templateFields[id].Template1 : {});
-    //console.log(templateFields);
     const titleFieldId = `case-block-title-${id}`;
     const titleValue = templateFields[titleFieldId] || '';
     const descFieldId = `case-block-text-${id}`;
     const descValue = templateFields[descFieldId] || '';
     //templateFields['case-block-sectionId'] = id;
-    console.log(selectedMedia);
-
 
     return (
         <div className={`${styles['admin-tmp-row']} ${styles['admin-tmp1']}`}>
@@ -37,13 +32,19 @@ const Template1 = ({ id, handleInputChange, formData, handleMediaInputChange, se
                     inputPlaceholder="Add your title here..."
                     onChange={(event) => handleInputChange(titleFieldId, event.target.value, id, 'Template1')}
                 />
-                <DescBlock 
+                <QuillComponent
+                    value={descValue}
+                    id={descFieldId}
+                    name={`case-block-text-${id}`}
+                    onChange={(event) => handleInputChange(descFieldId, event, id, 'Template1')}
+                />
+                {/*<DescBlock
                     sectionTitle="Section Description"
                     sectionTitleVal={descValue}
                     inputID={descFieldId}
                     inputName={`case-block-text-${id}`}
                     onChange={(event) => handleInputChange(descFieldId, event.target.value, id, 'Template1')}
-                />
+                />*/}
             </div>
         </div>
     )
