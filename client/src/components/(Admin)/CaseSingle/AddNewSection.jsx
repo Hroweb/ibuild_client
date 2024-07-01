2a02cd7bbbd8381c5d7941b799bcde7c9e990097
anadevgh <anahit@ipoint.com.mt>
2024-07-01 21:40:54 +0400

    case add new section duplication

--- client/src/components/(Admin)/CaseSingle/AddNewSection.jsx
+++ client/src/components/(Admin)/CaseSingle/AddNewSection.jsx
@@ -122,40 +122,51 @@ const AddNewSection = ({ updateTemplates }) => {
                 </div>
             </div>
             {sections.map((section, index) => (
-                <div className={`${styles['admin-dp-frow']}`} key={section.id} data-key={section.id}>
-                    <div className={`${styles['admin-dp-row']}`}>
-                        <div className={`${styles['admin-dp-col']} ${section.dropdownOpen ? styles['opened'] : ''}`}>
-                            <div className={`${styles['admin-dp-toggle']} fx fx-jb fx-ac`} onClick={() => toggleDropdown(section.id)}>
-                                <span>{section.templateText || 'Choose a template'}</span>
-                                <ArrDown />
-                            </div>
-                            {section.dropdownOpen && (
-                                <div ref={section.ref} className={`${styles['admin-dp-list']}`}>
-                                    <a data-opt="template1" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template1', 'T1 - 1 column (1 image & text)')}>T1 - 1 column (1 image & text)</a>
-                                    <a data-opt="template2" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template2', 'T2 - Gif Animation')}>T2 - Gif Animation</a>
-                                    <a data-opt="template3" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template3', 'T3 - 2 column (2 images, 1 fullwidth text)')}>T3 - 2 column (2 images, 1 fullwidth text)</a>
-                                    <a data-opt="template4" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template4', 'T4 - 2 column (2 images, 2 fullwidth text)')}>T4 - 2 column (2 images, 2 fullwidth text)</a>
-                                    <a data-opt="template5" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template5', 'T5 - 1 column (only image)')}>T5 - 1 column (only image)</a>
-                                    <a data-opt="template6" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template6', 'T6 - 2 column (only image)')}>T6 - 2 column (only image)</a>
-                                    <a data-opt="template7" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template7', 'T7 - 1 column (alternative option)')}>T7 - 1 column (alternative option)</a>
+                <div key={section.id}>
+                    <div className={`${styles['admin-dp-frow']}`} key={section.id} data-key={section.id}>
+                        <div className={`${styles['admin-dp-row']}`}>
+                            <div className={`${styles['admin-dp-col']} ${section.dropdownOpen ? styles['opened'] : ''}`}>
+                                <div className={`${styles['admin-dp-toggle']} fx fx-jb fx-ac`} onClick={() => toggleDropdown(section.id)}>
+                                    <span>{section.templateText || 'Choose a template'}</span>
+                                    <ArrDown />
                                 </div>
-                            )}
+                                {section.dropdownOpen && (
+                                    <div ref={section.ref} className={`${styles['admin-dp-list']}`}>
+                                        <a data-opt="template1" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template1', 'T1 - 1 column (1 image & text)')}>T1 - 1 column (1 image & text)</a>
+                                        <a data-opt="template2" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template2', 'T2 - Gif Animation')}>T2 - Gif Animation</a>
+                                        <a data-opt="template3" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template3', 'T3 - 2 column (2 images, 1 fullwidth text)')}>T3 - 2 column (2 images, 1 fullwidth text)</a>
+                                        <a data-opt="template4" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template4', 'T4 - 2 column (2 images, 2 fullwidth text)')}>T4 - 2 column (2 images, 2 fullwidth text)</a>
+                                        <a data-opt="template5" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template5', 'T5 - 1 column (only image)')}>T5 - 1 column (only image)</a>
+                                        <a data-opt="template6" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template6', 'T6 - 2 column (only image)')}>T6 - 2 column (only image)</a>
+                                        <a data-opt="template7" className="fx fx-ac fx-jb" onClick={() => handleTemplateSelection(section.id, 'Template7', 'T7 - 1 column (alternative option)')}>T7 - 1 column (alternative option)</a>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        {section.templateKey && (
+                            <div className={`${styles['admin-tmp-block']}`}>
+                                <Suspense fallback={<LoadingAnim />}>
+                                    {React.createElement(templates[section.templateKey], {
+                                        key: section.id,
+                                        id: section.id,
+                                        handleInputChange: handleInputChangeWrapper,
+                                        handleMediaInputChange: handleMediaInputChangeWrapper,
+                                        formData: formData,
+                                        selectedMedia: selectedMedia
+                                    })}
+                                </Suspense>
+                            </div>
+                        )}
+                    </div>
+                    <div className={`${styles['addNew-cs-row']}`} onClick={addSection}>
+                        <div className="fx fx-ac fx-jb">
+                            <span>Add Another Custom Section</span>
+                            <div className={`${styles['addNew-toggle']}`}>
+                                <div></div>
+                                <div></div>
+                            </div>
                         </div>
                     </div>
-                    {section.templateKey && (
-                        <div className={`${styles['admin-tmp-block']}`}>
-                            <Suspense fallback={<LoadingAnim />}>
-                                {React.createElement(templates[section.templateKey], {
-                                    key: section.id,
-                                    id: section.id,
-                                    handleInputChange: handleInputChangeWrapper,
-                                    handleMediaInputChange: handleMediaInputChangeWrapper,
-                                    formData: formData,
-                                    selectedMedia: selectedMedia
-                                })}
-                            </Suspense>
-                        </div>
-                    )}
                 </div>
             ))}
         </>
