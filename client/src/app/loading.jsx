f10ffbc34596ad61d15e7f037c49e6a5d81a09a4
Hroweb <hroweb89@gmail.com>
2024-07-08 08:41:08 +0400

    testing loading

--- client/src/app/loading.jsx
+++ client/src/app/loading.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState, useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
 import loadingAnim from '@animations/loader-v2.json';
 import '@/app/_loading.scss';
 
@@ -7,82 +8,31 @@ const Loading = () => {
     const [loadingVisible, setLoadingVisible] = useState(true);
     const [loadingProgress, setLoadingProgress] = useState(0);
     const containerRef = useRef(null);
-    const animationInstance = useRef(null);
 
     useEffect(() => {
-        let totalResources = 0;
-        let loadedResources = 0;
+        const animation = lottie.loadAnimation({
+            container: containerRef.current,
+            animationData: loadingAnim,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+        });
 
-        const updateProgress = () => {
-            const progress = Math.min((loadedResources / totalResources) * 100, 100);
+        let progress = 0;
+
+        const interval = setInterval(() => {
             setLoadingProgress(progress);
-
-            if (loadedResources >= totalResources) {
-                setLoadingVisible(true);
+            progress += 1;
+            if (progress > 100) {
+                clearInterval(interval);
+                setLoadingVisible(false);
             }
+        }, 50);
+
+        return () => {
+            animation.destroy();
+            clearInterval(interval);
         };
-
-        const handleResourceLoad = () => {
-            loadedResources += 1;
-            updateProgress();
-        };
-
-        const loadLottie = async () => {
-            const lottie = (await import('lottie-web')).default;
-
-            if (!animationInstance.current) {
-                animationInstance.current = lottie.loadAnimation({
-                    container: containerRef.current,
-                    animationData: loadingAnim,
-                    renderer: 'svg',
-                    loop: true,
-                    autoplay: true,
-                });
-            }
-
-            const resources = document.querySelectorAll('img, video, iframe, script, link[rel="stylesheet"]:not([href*="/admin/"])');
-            totalResources = resources.length;
-
-            if (totalResources === 0) {
-                setLoadingVisible(true);
-                return;
-            }
-
-            resources.forEach((resource) => {
-                if (resource.complete) {
-                    handleResourceLoad();
-                } else {
-                    resource.addEventListener('load', handleResourceLoad);
-                    resource.addEventListener('error', handleResourceLoad);
-                }
-            });
-
-            const smoothProgressIncrement = setInterval(() => {
-                setLoadingProgress((prev) => {
-                    if (prev < 100) {
-                        return Math.min(prev + 1, 100);
-                    } else {
-                        clearInterval(smoothProgressIncrement);
-                        return prev;
-                    }
-                });
-            }, 100);
-
-            return () => {
-                if (animationInstance.current) {
-                    animationInstance.current.destroy();
-                    animationInstance.current = null;
-                }
-                clearInterval(smoothProgressIncrement);
-                resources.forEach((resource) => {
-                    resource.removeEventListener('load', handleResourceLoad);
-                    resource.removeEventListener('error', handleResourceLoad);
-                });
-            };
-        };
-
-        loadLottie().then(() => '');
-
     }, []);
 
     return loadingVisible ? (
@@ -90,7 +40,7 @@ const Loading = () => {
             <div className="loading-wrapper">
                 <div className="loader-anim lt-anim-ts" ref={containerRef}></div>
                 <div className="loader-txt fx fx-wrap">
-                    <span>{`${Math.floor(loadingProgress)}%`}</span>
+                    <span>{`${loadingProgress}%`}</span>
                     <div className="loader-bar" style={{ width: `${loadingProgress}%` }}></div>
                 </div>
             </div>
