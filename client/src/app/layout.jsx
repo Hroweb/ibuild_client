962c1dffaba7bb9d70cab2dd317f7b3edc7c4609
anadevgh <anahit@ipoint.com.mt>
2024-08-02 10:30:14 +0400

    google consent test2

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -41,6 +41,8 @@ export default function RootLayout({ children }) {
 			{`
 				window.dataLayer = window.dataLayer || [];
 				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', 'G-W25S3RRHLL');
 				gtag('consent', 'default', {
 					'ad_storage': 'denied',
 					'analytics_storage': 'denied',
