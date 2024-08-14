012f5b17a4823b291e6dfb1961f9fe3e69c38830
anadevgh <anahit@ipoint.com.mt>
2024-08-14 12:08:00 +0400

    gtag update new v1

--- client/src/context/ConsentContext.js
+++ client/src/context/ConsentContext.js
@@ -6,21 +6,7 @@ const ConsentContext = createContext();
 export function ConsentProvider({ children }) {
     const updateConsent = (consentValues) => {
         if (typeof window !== "undefined" && window.gtag) {
-            const defaultConsent = {
-                'ad_storage': 'denied',
-                'ad_user_data': 'denied',
-                'ad_personalization': 'denied',
-                'analytics_storage': 'denied',
-                'functionality_storage': 'denied',
-                'personalization_storage': 'denied',
-                'security_storage': 'granted',
-				'wait_for_update': 500
-            };
-
-            // Merge the provided consent values with the defaults
-            const updatedConsent = { ...defaultConsent, ...consentValues };
-
-            window.gtag('consent', 'update', updatedConsent);
+            window.gtag('consent', 'update', consentValues);
         }
     };
 
